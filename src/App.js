6ffff2ded8fcb692ec21aa183a2b268b5b4f4f86
Priyank Arya <aryapriyank@vt.edu>
2023-10-18 01:54:09 -0400

    toggle side menu bar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Reports from './pages/Reports';
+import Products from './pages/Products';
+
 
 function App() {
   return (
-    <div className="App">
+    <>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/reports' element={<Reports/>} />
+        <Route path='/products' element={<Products/>} />
+      </Routes>
+    </Router>
+    {/* <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <h1>App</h1>
       </header>
-    </div>
+    </div> */}
+    </>
   );
 }
 
