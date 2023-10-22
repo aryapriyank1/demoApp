f18f5460147aaf79afd63f8b5c80cc6e53886c18
Priyank Arya <aryapriyank@vt.edu>
2023-10-22 02:35:28 -0400

    Implementing Grid Layout

--- src/pages/DragDrop.js
+++ src/pages/DragDrop.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { DndProvider } from 'react-dnd';
-// import Backend from "react-dnd-html5-backend";
 import { HTML5Backend as Backend } from 'react-dnd-html5-backend';
 import Example from '../components/DragDrop/example';
 import '../components/DragDrop/styles.css';
